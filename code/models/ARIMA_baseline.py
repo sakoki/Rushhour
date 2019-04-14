173bed0ef94bd5b187dc7bf00c5a821e74fe51d3
G-bgyl <alicia17ge@gmail.com>
2019-04-14 13:49:34 -0400

    finish arima pipeline arima and minor change on lstm pipeline

--- code/models/ARIMA_baseline.py
+++ code/models/ARIMA_baseline.py
@@ -5,9 +5,15 @@ from statsmodels.tsa.arima_model import ARIMA
 import numpy as np
 import os,sys
 import pandas as pd
+import pprint
+
+sys.path.insert(0,os.getcwd()+'/code/utils/')
+
+# own functions
+from toolkit import load_pickle
 
 # from code snippet: https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/
-def predict_time_series_ARIMA(mean_series, title, fig_size, split_size=0.9, full=False):
+def predict_time_series_ARIMA(mean_series, title=None, fig_size=(20, 10), split_size=0.25, full=False):
     labels = mean_series.index  # add label
 
     X = mean_series.values
@@ -31,52 +37,70 @@ def predict_time_series_ARIMA(mean_series, title, fig_size, split_size=0.9, full
         indexes.append(test_label[t])  # add label
     #         if t %100 ==0:
     #             print('predicted=%f, expected=%f' % (yhat, obs))
-    mse = mean_squared_error(test, predictions)
+    rmse = np.sqrt(mean_squared_error(test, predictions))
     mae = mean_absolute_error(test, predictions)
-    print('Test RMSE: %.3f' % np.sqrt(mse))
+    print('Region id: %s'%(region_id))
+    print('Test RMSE: %.3f' % np.sqrt(rmse))
     print('Test MAE: %.3f' % mae)
 
-    # plot
-    sns.set(rc={'figure.figsize': fig_size})
-    if full:
-        plt.plot(labels, X, label='train data')
-    #         plt.plot(labels[int(len(labels)/2):],X[int(len(X)/2):])
-    elif full == False:
-        plt.plot(indexes, test)
-    else:#(full == None)
-        return mse, mae
-    plt.plot(indexes, predictions, color='red', label='validation data')  #
-    plt.legend()
-    plt.title(title)
+    if full==None:
+        return rmse, mae
+    else:
+        # plot
+        sns.set(rc={'figure.figsize': fig_size})
+        if full:
+            plt.plot(labels, X, label='train data')
+        #         plt.plot(labels[int(len(labels)/2):],X[int(len(X)/2):])
+        elif full == False:
+            plt.plot(indexes, test)
 
-    return plt, mse, mae
+        plt.plot(indexes, predictions, color='red', label='validation data')  #
+        plt.legend()
+        plt.title(title)
+
+        return plt, rmse, mae
 
 if __name__=="__main__":
 
-    file_name = sys.argv[1] # 'region_by_time_series.csv'
+    file_name = sys.argv[1] # 'region_time_series_filtered.csv'
     output_root = os.getcwd()
 
     new_time_df_new = pd.read_csv(output_root+'/output/'+file_name, index_col = 0)
 
-    mse_list, mae_list = [], []
+    rmse_list, mae_list = [], []
+
     for region_id, row in new_time_df_new.iterrows():
-        mse, mae = predict_time_series_ARIMA(row,  split_size=0.7, full=None)# title=region_id, fig_size=(20, 10),
-        mse_list.append(mse)
+        rmse, mae = predict_time_series_ARIMA(row, title=region_id,fig_size=None, split_size=0.7, full=None)
+        rmse_list.append(rmse)
         mae_list.append(mae)
 
-    plt.plot(mse_list)
-    plt.title('model MSE')
-    plt.ylabel('MSE')
-    plt.xlabel('epoch')
-    plt.legend(['ARIMA_mse'], loc='upper left')
-    plt.savefig('output/arima_mse.png')
-    plt.show()
 
 
-    plt.title('model MAE')
-    plt.ylabel('MAE')
-    plt.xlabel('epoch')
-    plt.legend(['ARIMA_mae'], loc='upper left')
-    plt.savefig('output/arima_mae.png')
-    plt.show()
+    # weighted average
+    weights = load_pickle("region_weights.p",path = 'output/')
+    pprint.pprint(weights)
+    weight_list = list(weights.values())
+
+
+    # weighted mae
+    weighted_mae = np.average(mae_list,weights = weight_list)
+    #weighted rmse
+    weighted_rmse = np.average(rmse_list, weights=weight_list)
+    print('weighted_rmse:%s\nweighted_mae:%s'%(weighted_rmse,weighted_mae))
+
+    # plt.plot(rmse_list)
+    # plt.title('model RMSE')
+    # plt.ylabel('RMSE')
+    # plt.xlabel('epoch')
+    # plt.legend(['ARIMA_rmse'], loc='upper left')
+    # plt.savefig('output/arima_rmse.png')
+    # plt.show()
+    #
+    #
+    # plt.title('model MAE')
+    # plt.ylabel('MAE')
+    # plt.xlabel('epoch')
+    # plt.legend(['ARIMA_mae'], loc='upper left')
+    # plt.savefig('output/arima_mae.png')
+    # plt.show()
 
--- code/utils/toolkit.py
+++ code/utils/toolkit.py
@@ -1,5 +1,6 @@
 import re
 import os
+import pickle
 
 def get_fname(path, contains=False):
     file = [f for f in os.listdir(path) if re.search('(.+\.csv$)', f)]
@@ -17,12 +18,18 @@ def check_dir_exist(path):
         command = 'mkdir -p {}'.format(path)
         os.system(command)
 
-# TO DO: defined twice
-# Consider removing
-def generate_fname_wPath(DIR, region_id, attr=False):
+
+def generate_fname_wPath(DIR, attr1, attr2=False):
     """Generate file name with whole path"""
-    # avoid file name with "'"
-    # if "'" in attr:
-    #   attr = attr.replace("'", "_")
-    fname_wPath = '%s/%s_%s.csv'%(DIR,attr,region_id)
+    fname_wPath = '%s/%s_%s.csv'%(DIR,attr2,attr1)
     return fname_wPath
+
+
+def save_pickle(variable, f_name,path=False):
+    pickle.dump(variable, open("%s%s"%(path,f_name),"wb"))
+    print('successfully dump %s'%(f_name))
+
+def load_pickle(f_name,path=False):
+    variable=pickle.load(open(r"%s%s"%(path,f_name), "rb"))
+    print('successfully load %s'%(f_name))
+    return variable
